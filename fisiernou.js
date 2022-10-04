97d5e04a568d551f0eced204d278a5e787aed58d
elenavisan <elenavisan2001@gmail.com>
2022-10-04 17:50:11 +0300

    seminar2

---
+++ fisiernou.js
@@ -0,0 +1,16 @@
+/*console.log(process.argv[0]);
+console.log(process.argv[1]);
+console.log(process.argv[2]);*/
+
+/*let sayHello = function (name) {
+    return "hello, " + name;
+}
+console.log(sayHello("Elena"));*/
+
+/*let sayHello = (name) => "Hello, " + name;
+
+console.log(sayHello("me"));*/
+
+let sayHello = (name) => "Hello, " + name;
+
+console.log(sayHello(process.argv[2]));
